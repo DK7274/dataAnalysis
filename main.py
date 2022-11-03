38154af78b741193fc65b0a7ab7eecae0f261276
Darcy Keys <dkeys8722@gmail.com>
2022-11-03 12:23:12 +1100

    penis mode

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+#importing the required libraries
 import matplotlib.pyplot as plt
+import numpy as np
 
-fig, ax = plt.subplots()
-ax.plot([1,2,3,4],[1,4,2,5])
-plt.ylabel('some numbers')
-plt.xlabel('some things')
-plt.show()
+#define data values
+x = np.array([1,1,2,2,1,4,4,3,3,3,2,2]) #x-axis points
+y = np.array([1,2,2,1,1,1,2,2,1,4,4,1]) #Y-axis points
+
+plt.plot(x,y) #plot the chart
+plt.xlabel("ball size") #add x-axis label
+plt.ylabel("length (inches)") #add y-axis label
+plt.title("penis") #add title
+plt.show() #display
