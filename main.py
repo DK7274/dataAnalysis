9fb5ce8cad3daf6240cd52d36d4bdb05d3334125
Darcy Keys <dkeys8722@gmail.com>
2022-11-03 12:24:41 +1100

    using numpy

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #define data values
-x = np.array([1,1,2,2,1,4,4,3,3,3,2,2]) #x-axis points
-y = np.array([1,2,2,1,1,1,2,2,1,4,4,1]) #Y-axis points
+x = np.array([1,2,3,4]) #x-axis points
+y = x*2 #Y-axis points
 
 plt.plot(x,y) #plot the chart
-plt.xlabel("ball size") #add x-axis label
-plt.ylabel("length (inches)") #add y-axis label
-plt.title("penis") #add title
+plt.xlabel("X-axis") #add x-axis label
+plt.ylabel("Y-axis") #add y-axis label
+plt.title("Chart") #add title
 plt.show() #display
